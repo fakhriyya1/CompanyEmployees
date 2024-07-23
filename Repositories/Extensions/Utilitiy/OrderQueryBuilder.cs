3bc27d2d941329bbb9096a0963c896a34d2f976e
fakhriyya1 <bagirovafexriyye5@gmail.com>
2024-07-23 20:47:45 +0400

    improving sorting functionslity

---
+++ Repositories/Extensions/Utilitiy/OrderQueryBuilder.cs
@@ -0,0 +1,39 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Extensions.Utilitiy
+{
+    public static class OrderQueryBuilder
+    {
+        public static string CreateOrderQuery<T>(string orderByQueryString)
+        {
+            var orderParams = orderByQueryString.Split(',');  //getting individual fields
+            var properyInfos = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);  //using reflection to get employee properties
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
+
+                var propertyFromQueryName = param.Trim(' ').Split(" ")[0];
+                var objectProperty = properyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+
+                if (objectProperty == null)
+                    continue;
+
+                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}, ");
+            }
+
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+            return orderQuery;
+        }
+    }
+}
